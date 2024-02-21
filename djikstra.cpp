36d35a2bf786aa4edff18f52fcf128651a05e77e
Leandro <devleandromcz@gmail.com>
2024-02-21 14:15:30 -0300

    Sla

--- djikstra.cpp
+++ djikstra.cpp
@@ -12,13 +12,20 @@ int main() {
     
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> arestas(m, vector<int>(n, 0));
+    vector<vector<int>> arestas(m, vector<int>(3));
     for (int i = 0; i < m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        arestas[i][0] = a;
-        arestas[i][1] = b;
-        arestas[i][2] = c;
+        // arestas[i][0] = a;
+        // arestas[i][1] = b;
+        // arestas[i][2] = c;
+        arestas[i].push_back(a);
+        arestas[i].push_back(b);
+        arestas[i].push_back(c);
+    }
+
+    for (int i = 0; i < m; i++) {
+        cout << arestas[i][0] << " " << arestas[i][1] << " " << arestas[i][2] << endl;
     }
 
     
