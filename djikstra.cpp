5216f864ac4a3848fecaeaa504c51fcd0cdb4dc4
Leandro <devleandromcz@gmail.com>
2024-02-21 13:52:22 -0300

    Get vertexs and edges

--- djikstra.cpp
+++ djikstra.cpp
@@ -1,10 +1,30 @@
+// g++ hello.cpp -o hello
+// ./hello
+
 #include <iostream>
 #include <string>
+#include <vector>   
 
 using namespace std;
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arestas(m, vector<int>(n, 0));
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        arestas[i][0] = a;
+        arestas[i][1] = b;
+        arestas[i][2] = c;
+    }
+
+    
+
+
+
+
     return 0;
 }
