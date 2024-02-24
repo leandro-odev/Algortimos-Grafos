a44f07164202ba198904d996e86c5e4f2268d147
Leandro <devleandromcz@gmail.com>
2024-02-24 15:10:04 -0300

    Kosaraju version that shows the strongly connected commponentes

---
+++ kosaraju2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+void dfs(int node, vector<int> &vis, vector<int> adj[], stack<int> &st) {
+    vis[node] = 1;
+    for (auto it : adj[node]) {
+        if (!vis[it]) {
+            dfs(it, vis, adj, st);
+        }
+    }
+    st.push(node);
+}
+
+void dfs_reverse(int node, vector<int> &vis, vector<int> adj[], vector<int> &component) {
+    vis[node] = 1;
+    component.push_back(node);
+    for (auto it : adj[node]) {
+        if (!vis[it]) {
+            dfs_reverse(it, vis, adj, component);
+        }
+    }
+}
+
+int kosaraju(int V, vector<int> adj[], vector<vector<int>> &sccs) {
+    vector<int> vis(V, 0);
+    stack<int> st;
+
+    for (int i = 0; i < V; i++) {
+        if (!vis[i]) {
+            dfs(i, vis, adj, st);
+        }
+    }
+
+    vector<int> adjT[V];
+    for (int i = 0; i < V; i++) {
+        vis[i] = 0;
+        for (auto it : adj[i]) {
+            adjT[it].push_back(i);
+        }
+    }
+
+    while (!st.empty()) {
+        int node = st.top();
+        st.pop();
+        if (!vis[node]) {
+            vector<int> component;
+            dfs_reverse(node, vis, adjT, component);
+            sccs.push_back(component);
+        }
+    }
+    return sccs.size();
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> adj[n];
+    for (int i = 0; i < m; ++i) {
+        int v1, v2, w;
+        cin >> v1 >> v2 >> w;
+        adj[v1].push_back(v2);
+    }
+
+    vector<vector<int>> sccs;
+    int ans = kosaraju(n, adj, sccs);
+
+    cout << "The number of strongly connected components is: " << ans << endl;
+
+    cout << "Strongly connected components:\n";
+    for (int i = 0; i < sccs.size(); ++i) {
+        cout << "Component " << i + 1 << ": ";
+        for (int j = 0; j < sccs[i].size(); ++j) {
+            cout << sccs[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
